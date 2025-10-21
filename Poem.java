77c1bfea69c6ae06ca8caf90122b15624ef56066
MishanRana1 <mishanrana12@gmail.com>
2025-10-21 19:51:13 +0000

    Added to Poem class.

---
+++ Poem.java
@@ -0,0 +1,19 @@
+public class Poem {
+
+    private String name;
+    private String poet;
+
+    public Poem(String name, String poet) {
+        this.name = name;
+        this.poet = poet;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPoet() {
+        return poet;
+    }
+
+}
